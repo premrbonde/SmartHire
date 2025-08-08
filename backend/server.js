46aecfeef5c7cdd7c1d4c5c5ba6697503f873812
Prem Bonde <123545122+premrbonde@users.noreply.github.com>
2025-08-08 12:15:35 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import app from "./app.js";
 import cloudinary from "cloudinary";
-
+import cors from 'cors';
 cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME, // CLOUDINARY_CLIENT_NAME
   api_key: process.env.CLOUDINARY_API_KEY,       // CLOUDINARY_CLIENT_API
@@ -10,3 +10,7 @@ console.log('⚙️  Using DB URL:', process.env.DB_URL);
 app.listen(process.env.PORT, () => {
   console.log(`Server running at port ${process.env.PORT}`);
 });
+app.use(cors({
+  origin: 'https://your-vercel-app.vercel.app',
+  credentials: true
+}));
